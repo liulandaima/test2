9960dd6a0befec286acfcac7b52202f846d67eda
xiefaguo <xiefaguo@com.cn>
2024-03-16 13:42:05 +0800

    换行

--- shop-activity/shop-activity-api/src/main/java/quick/pager/shop/Activity.java
+++ shop-activity/shop-activity-api/src/main/java/quick/pager/shop/Activity.java
@@ -14,6 +14,7 @@ import quick.pager.shop.mq.ActivityMQ;
 @EnableBinding(ActivityMQ.class)
 public class Activity {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Activity.class, args);
     }
